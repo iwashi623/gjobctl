238b914e0ba43bdd3e38c5040643b657098b95d6
k-iwashita-prtimes <wonderland.7878@gmail.com>
2023-01-30 19:24:27 +0900

    Completed create function template

--- create.go
+++ create.go
@@ -3,6 +3,7 @@ package gjobctl
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,12 +12,12 @@ import (
 )
 
 type CreateOption struct {
-	JobScriptFile *string `short:"f" long:"job-script-file" description:"Job script file path" required:"true"`
+	JobSettingFile *string `arg:"" name:"job-setting-file" description:"job setting file in json" required:"true"`
 }
 
 func (app *App) Create(opt *CreateOption) error {
 	// JSONファイルからGlue Jobの設定を読み込む
-	f, err := os.Open(*opt.JobScriptFile)
+	f, err := os.Open(*opt.JobSettingFile)
 	if err != nil {
 		return err
 	}
@@ -29,8 +30,6 @@ func (app *App) Create(opt *CreateOption) error {
 		return err
 	}
 
-	fmt.Println(job)
-
 	// AWSセッションを作成
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: &app.config.Region},
@@ -41,20 +40,31 @@ func (app *App) Create(opt *CreateOption) error {
 
 	// Glue Jobを更新
 	r, err := sv.CreateJob(&glue.CreateJobInput{
-		Name:              job.Name,
-		Command:           job.Command,
-		Connections:       job.Connections,
-		DefaultArguments:  job.DefaultArguments,
-		Description:       job.Description,
-		ExecutionProperty: job.ExecutionProperty,
-		LogUri:            job.LogUri,
-		MaxRetries:        job.MaxRetries,
-		Role:              job.Role,
+		CodeGenConfigurationNodes: job.CodeGenConfigurationNodes,
+		Command:                   job.Command,
+		Connections:               job.Connections,
+		DefaultArguments:          job.DefaultArguments,
+		Description:               job.Description,
+		ExecutionClass:            job.ExecutionClass,
+		ExecutionProperty:         job.ExecutionProperty,
+		GlueVersion:               job.GlueVersion,
+		LogUri:                    job.LogUri,
+		MaxRetries:                job.MaxRetries,
+		Name:                      job.Name,
+		NonOverridableArguments:   job.NonOverridableArguments,
+		NotificationProperty:      job.NotificationProperty,
+		NumberOfWorkers:           job.NumberOfWorkers,
+		Role:                      job.Role,
+		SecurityConfiguration:     job.SecurityConfiguration,
+		SourceControlDetails:      job.SourceControlDetails,
+		Timeout:                   job.Timeout,
+		WorkerType:                job.WorkerType,
 	})
 	if err != nil {
 		return fmt.Errorf("failed to create Glue Job: %w", err)
 	}
 
-	fmt.Println("Successfully created Glue Job: " + *r.Name)
+	log.Println("Successfully created Glue Job: " + *r.Name)
+	log.Println(job)
 	return nil
 }
